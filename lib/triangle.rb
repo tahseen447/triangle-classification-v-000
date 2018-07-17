d5140eff56590390f0a79c393046539d7dfda1a0
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-17 18:35:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here  
+  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a= a
+    @b= b
+    @c = c
+    end
+
+    def kind
+      if @a != @b != @c
+        :equilateral
+      elsif @a==@b || @b==@c || @a==@case 
+        :isosceles
+      elsif
+        
+      end
 end
