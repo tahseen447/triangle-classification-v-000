8b0f960ad0b01da0a35042e0ee1828ac344e6e46
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-17 18:46:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
     end
 
     def kind
-      if @a < 0 || @b <0 || @c <0
+      if valid_traingle?
         begin
           raise TriangleError
         rescue TraingeError => error
