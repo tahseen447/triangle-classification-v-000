c0776b4cbd9fbaa7d9caade75fb81161b0b68125
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-17 18:52:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
     end
 
     def valid_traingle?
-      @a> 0 && @b >0 && @c >0 && @a+@b >@c && @b+@c > @a && @a+@c > @b
+      @a> 0 && @b >0 && @c >=0 && @a+@b >@c && @b+@c > @a && @a+@c > @b
       end
 
     class TraingeError < StandardError
